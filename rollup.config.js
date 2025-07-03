f10c7f056ad26aa856282cbbb6b01aca4205e294
im10anish <im10anish@gmail.com>
2025-07-03 21:53:06 +0530

    fix: update build file

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const postcss = require("rollup-plugin-postcss");
+const { default: dts } = require("rollup-plugin-dts");
 
 const packageJson = require("./package.json");
 
-export default [
+module.exports = [
   {
     input: "src/index.ts",
     output: [
